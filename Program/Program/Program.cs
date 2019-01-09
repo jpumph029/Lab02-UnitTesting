cb727731a35c83e4dbee895ef6fc7fa2c477bbb0
Joseph <pumphrey.joseph@gmail.com>
2019-01-09 15:38:39 -0800

    made the ViewBalance, and WithdrawBalance methods

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -4,12 +4,14 @@ namespace Program
 {
     class Program
     {
+        public static double Balance = 200;
+
         static void Main(string[] args)
         {
-            UserInterFace();
+            UserInterface();
         }
 
-        static void UserInterFace()
+        static void UserInterface()
         {
             Console.WriteLine(@"
       BIG JOES 
@@ -21,6 +23,7 @@ namespace Program
   \__,_|\__|_| |_| |_|");
             Menu();
         }
+
         static void Menu()
         {
             Console.WriteLine(@"
@@ -28,35 +31,85 @@ namespace Program
    (1) Withdraw
    (2) Deposit
    (3) Exit");
-            int menuInput = Convert.ToInt32(Console.ReadLine());
-            MenuOptions(menuInput);
+            try
+            {
+                int menuInput = Convert.ToInt32(Console.ReadLine());
+                MenuOptions(menuInput);
+            }
+            catch (Exception)
+            {
+                Console.Write("You broke it. Try again");
+                Console.ReadKey();
+                Console.Clear();
+            }
+                    // View Balance 
+            finally
+            {
+                UserInterface();
+            }
+            
         }
         static void MenuOptions(int menuInput)
         {
             switch (menuInput)
             {
                 case 0:
-                    Console.WriteLine("Test0");
-                    // View Balance Method
+                    ViewBalance(Balance);
+                    Console.ReadKey();
+                    Console.Clear();
                     break;
                 case 1:
-                    Console.WriteLine("Test1");
-                    //Withdraw Money Method
+                    WithdrawBalance(Balance);
+                    Console.ReadKey();
+                    Console.Clear();
+                    
+                    //Withdraw Money
                     break;
                 case 2:
-                    Console.WriteLine("Test2");
+                    Console.Write("Test2");
+                    Console.ReadKey();
+                    Console.Clear();
                     // Deposit Money Method
                     break;
                 case 3:
-                    Console.WriteLine("Test3");
+                    Console.Write("Test3");
+                    Console.ReadKey();
+                    Console.Clear();
                     // Exit Method
                     break;
                 default:
-                    Console.WriteLine("Test4");
+                    Console.Write("You broke it. Try again");
+                    Console.ReadKey();
+                    Console.Clear();
                     break;
-
             }
+        }
 
+        public static double ViewBalance(double Balance)
+        {
+            Console.WriteLine($"Your current balance is: ${Balance}");
+            return Balance;
+        }
+        public static double WithdrawBalance(double Balance)
+        {
+            Console.WriteLine("Type an amount of money to withdraw: ");
+            string userInput = Console.ReadLine();
+            double sysInput = Convert.ToDouble(userInput);
+            if (sysInput > Balance)
+            {
+                Console.WriteLine("Insignificant funds.");
+            }
+            else if (sysInput < 0)
+            {
+                Console.WriteLine("Cheater");
+            } 
+            else
+	        {
+                Balance = Balance - sysInput;
+                Console.WriteLine($"Your remaining balance is: ${Balance}");
+            }
+            
+            return Balance;
         }
     }
 
