7d6e47476f42209d26ce6b098f1eb3ae48ce68a2
Joseph <pumphrey.joseph@gmail.com>
2019-01-09 13:14:47 -0800

    Made a user interface with a working menu

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -6,7 +6,58 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserInterFace();
+        }
+
+        static void UserInterFace()
+        {
+            Console.WriteLine(@"
+      BIG JOES 
+        _             
+       | |            
+   __ _| |_ _ __ ___  
+  / _` | __| '_ ` _ \ 
+ | (_| | |_| | | | | |
+  \__,_|\__|_| |_| |_|");
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine(@"
+   (0) View Balance
+   (1) Withdraw
+   (2) Deposit
+   (3) Exit");
+            int menuInput = Convert.ToInt32(Console.ReadLine());
+            MenuOptions(menuInput);
+        }
+        static void MenuOptions(int menuInput)
+        {
+            switch (menuInput)
+            {
+                case 0:
+                    Console.WriteLine("Test0");
+                    // View Balance Method
+                    break;
+                case 1:
+                    Console.WriteLine("Test1");
+                    //Withdraw Money Method
+                    break;
+                case 2:
+                    Console.WriteLine("Test2");
+                    // Deposit Money Method
+                    break;
+                case 3:
+                    Console.WriteLine("Test3");
+                    // Exit Method
+                    break;
+                default:
+                    Console.WriteLine("Test4");
+                    break;
+
+            }
+
         }
     }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab02-UnitTesting
+#The Problem Domain
+##Create a bank “ATM” machine within a console application with VS2017
+
+## Functionality
+User interface that prompts the user for standard ATM operations
+View Balance
+Withdraw Money
+Add Money (not standard, but for this exercise it will be….) =	- Keep asking the user to choose a transaction until they choose to ‘exit’ the application
