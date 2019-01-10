f7565dd96cb45749c1b6851704ac98a0d9f9c562
Joseph <pumphrey.joseph@gmail.com>
2019-01-09 18:37:14 -0800

    Base functionality done with all expetions caught

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -6,31 +6,28 @@ namespace Program
     {
         public static double Balance = 200;
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             UserInterface();
         }
-
         static void UserInterface()
         {
-            Console.WriteLine(@"
-      BIG JOES 
+            Console.WriteLine($@"
         _             
        | |            
    __ _| |_ _ __ ___  
   / _` | __| '_ ` _ \ 
  | (_| | |_| | | | | |
-  \__,_|\__|_| |_| |_|");
-            Menu();
-        }
+  \__,_|\__|_| |_| |_|
 
-        static void Menu()
-        {
-            Console.WriteLine(@"
+ Current Balance: ${Balance}
+ ----------------------
+
    (0) View Balance
    (1) Withdraw
    (2) Deposit
-   (3) Exit");
+   (3) Exit
+");
             try
             {
                 int menuInput = Convert.ToInt32(Console.ReadLine());
@@ -38,18 +35,17 @@ namespace Program
             }
             catch (Exception)
             {
-                Console.Write("You broke it. Try again");
+                Console.Write("Sorry something went wrong. Try again.\nPress Enter to Continue . . .");
                 Console.ReadKey();
                 Console.Clear();
             }
-                    // View Balance 
             finally
             {
                 UserInterface();
             }
             
         }
-        static void MenuOptions(int menuInput)
+        public static void MenuOptions(int menuInput)
         {
             switch (menuInput)
             {
@@ -57,28 +53,29 @@ namespace Program
                     ViewBalance(Balance);
                     Console.ReadKey();
                     Console.Clear();
+                    // View Balance 
                     break;
                 case 1:
-                    WithdrawBalance(Balance);
+                   Balance = WithdrawBalance(Balance);
                     Console.ReadKey();
                     Console.Clear();
                     
                     //Withdraw Money
                     break;
                 case 2:
-                    Console.Write("Test2");
+                    Balance = DepositToBalance(Balance);
                     Console.ReadKey();
                     Console.Clear();
                     // Deposit Money Method
                     break;
                 case 3:
-                    Console.Write("Test3");
+                    ExitProgram();
                     Console.ReadKey();
                     Console.Clear();
                     // Exit Method
                     break;
                 default:
-                    Console.Write("You broke it. Try again");
+                    Console.Write("Sorry something went wrong. Try again.\nPress any key to continue . . .");
                     Console.ReadKey();
                     Console.Clear();
                     break;
@@ -87,30 +84,51 @@ namespace Program
 
         public static double ViewBalance(double Balance)
         {
-            Console.WriteLine($"Your current balance is: ${Balance}");
+            Console.WriteLine($"Your current balance is: ${Balance}\nPress any key to continue . . .");
             return Balance;
         }
         public static double WithdrawBalance(double Balance)
         {
-            Console.WriteLine("Type an amount of money to withdraw: ");
+            Console.Write("Type an amount of money to withdraw: $");
             string userInput = Console.ReadLine();
             double sysInput = Convert.ToDouble(userInput);
             if (sysInput > Balance)
             {
-                Console.WriteLine("Insignificant funds.");
+                Console.WriteLine("Insignificant funds.\nPress any key to continue . . .");
             }
             else if (sysInput < 0)
             {
-                Console.WriteLine("Cheater");
+                Console.WriteLine("Cheater.\nPress any key to continue . . .");
             } 
             else
 	        {
                 Balance = Balance - sysInput;
-                Console.WriteLine($"Your remaining balance is: ${Balance}");
+                Console.WriteLine($"Your remaining balance is: ${Balance}\nPress Enter to continue . . .");
             }
             
             return Balance;
         }
-    }
+        public static double DepositToBalance(double Balance)
+        {
+            Console.Write("Type an amount of money to deposit: $");
+            string userInput = Console.ReadLine();
+            double sysInput = Convert.ToDouble(userInput);
+             if (sysInput < 0)
+            {
+                Console.WriteLine("Invalid.\nPress any key to continue . . .");
+            }
+            else
+            {
+                Balance = Balance + sysInput;
+                Console.WriteLine($"Your new balance is: ${Balance}\nPress any key to continue . . .");
+            }
 
+            return Balance;
+        }
+        public static void ExitProgram()
+        {
+            Environment.Exit(0);
+        }
+
+    }
 }
